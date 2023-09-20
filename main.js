e5583eda9620c4cb6f5fb64fcb6115029363f597
Kawan0606 <108882285+Kawan0606@users.noreply.github.com>
2023-09-20 12:37:08 -0300

    save

--- main.js
+++ main.js
@@ -12,6 +12,54 @@ function tocaSomClap () {
 
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
+//tecla Tim
+function tocaSomTim () {
+  document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+//tecla Puff
+function tocaSomPuff () {
+  document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+//tecla Splash
+function tocaSomSplash () {
+  document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+//tecla Toim
+function tocaSomToim () {
+  document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+//tecla Psh
+function tocaSomPsh () {
+  document.querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+//tecla Tic
+function tocaSomTic () {
+  document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+//tecla Tom
+function tocaSomTom () {
+  document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
 
 
 
